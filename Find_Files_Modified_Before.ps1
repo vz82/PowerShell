bf45394b9495e47fe01a3029ec72ac714b3d48ce
vz82 <44617142+vz82@users.noreply.github.com>
2025-12-02 21:44:04 +0100

    Add files via upload

---
+++ Find_Files_Modified_Before.ps1
@@ -0,0 +1,3 @@
+﻿#A one-liner for finding files (recursively) last MODIFIED BEFORE Jan. 31, 2019
+Get-ChildItem -Recurse | Where-Object { $_.LastWriteTime -lt "01/31/2019" }
+#Add this line to remove these also | Remove-Item -Force
