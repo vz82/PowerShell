64e60c9985e937e6bd140ee033cdd048226631e5
Vedran Zulin <44617142+vz82@users.noreply.github.com>
2025-12-04 22:09:10 +0100

    Add files via upload

---
+++ Firewall-Find_by_Name.ps1
@@ -0,0 +1,23 @@
+﻿#A one-liner searching through the ruleset by name
+#Name                          : CoreNet-IGMP-Out
+#DisplayName                   : Kernnetzwerk - Internetgruppenverwaltungs-Protokoll (IGMP ausgehend)
+#Description                   : IGMP-Nachrichten werden von Knoten gesendet und empfangen, um Multicastgruppen zu erstellen sowie Mitglieder hinzuzufügen und zu entfernen.
+#DisplayGroup                  : Kernnetzwerk
+#Group                         : @FirewallAPI.dll,-25000
+#Enabled                       : True
+#Profile                       : Any
+#Platform                      : {}
+#Direction                     : Outbound
+#Action                        : Allow
+#EdgeTraversalPolicy           : Block
+#LooseSourceMapping            : False
+#LocalOnlyMapping              : False
+#Owner                         : 
+#PrimaryStatus                 : OK
+#Status                        : Die Regel wurde erfolgreich vom Speicher aus analysiert. (65536)
+#EnforcementStatus             : NotApplicable
+#PolicyStoreSource             : PersistentStore
+#PolicyStoreSourceType         : Local
+#RemoteDynamicKeywordAddresses : {}
+#PolicyAppId                   :
+Get-NetFirewallrule -Name "CoreNet-IGMP-Out"
